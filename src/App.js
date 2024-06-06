b85afc3d63be63e0a8322eb4b2fed22c8d0ae2bb
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2024-06-06 12:21:44 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import Card from './components/Card';
+import Arr from './data'
 
 function App() {
+  
+  const newElem = Arr.map(item => {
+    return (
+      <Card key={item.id} item={item} />
+    )
+  })
   return (
     <div className="App">
       <Navbar />
       <Main />
-      <Card />
+      <div className="ComponentCard">
+        {newElem}
+      </div>
     </div>
 
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,36 +1,15 @@
-export default function Card(){
+export default function Card(props){
+    const badge = (props.item.openSpots === 0) ? 'SOLD OUT' : (props.item.location === "Online") ? 'ONLINE' : ''
     return(
-    <div className="ComponentCard">
         <div>
             <div className="relative">
-            <img src="./assets/card1.jpeg" alt="its a bike in the mountains"
-            className="ComponentCard__Img"/>
-            <p className="ComponentCard__P absolute">SOLD OUT</p>
+                <img src={`/images/${props.item.coverImg}`} alt={props.item.description}
+                className="ComponentCard__Img"/>
+                {badge && <p className="ComponentCard__P absolute">{badge}</p>}
             </div>
-            <p className="ComponentCard__P firstP">☆ 5.0 (30) USA</p>
-            <p className="ComponentCard__P SecondP">Life lessons with Katie Zaferes</p>
-            <p className="ComponentCard__P ThirdP"><span className="bold">From $136</span> / person</p>
+            <p className="ComponentCard__P firstP">☆{props.item.stats.rating} ({props.item.stats.reviewCount}) {props.item.location}</p>
+            <p className="ComponentCard__P SecondP">{props.item.title}</p>
+            <p className="ComponentCard__P ThirdP"><span className="bold">From ${props.item.price}</span> / person</p>
         </div>
-        <div>
-        <div className="relative">
-            <img src="./assets/card1.jpeg" alt="its a bike in the mountains"
-            className="ComponentCard__Img"/>
-            <p className="ComponentCard__P absolute">ONLINE</p>
-            </div>
-            <p className="ComponentCard__P firstP">☆ 5.0 (30) USA</p>
-            <p className="ComponentCard__P SecondP">Life lessons with Katie Zaferes</p>
-            <p className="ComponentCard__P ThirdP"><span className="bold">From $136</span> / person</p>
-        </div>
-        <div>
-        <div className="relative">
-            <img src="./assets/card1.jpeg" alt="its a bike in the mountains"
-            className="ComponentCard__Img"/>
-            <p className="ComponentCard__P absolute">ONLINE</p>
-            </div>
-            <p className="ComponentCard__P firstP">☆ 5.0 (30) USA</p>
-            <p className="ComponentCard__P SecondP">Life lessons with Katie Zaferes</p>
-            <p className="ComponentCard__P ThirdP"><span className="bold">From $136</span> / person</p>
-        </div>
-    </div>
     )
 }
